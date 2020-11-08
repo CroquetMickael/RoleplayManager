25a7aff20f96c537b71c1a4c8d30d51d86aca607
unknown <mickael.croquet@laposte.net>
2020-11-08 16:49:25 +0100

    feat(Index): Adding a lobby page

--- rpg-socket-client/src/Pages/Index/index.jsx
+++ rpg-socket-client/src/Pages/Index/index.jsx
@@ -1,91 +1,25 @@
-import React, { useEffect, useState, useContext } from "react";
-import { SocketContext } from "../Shared/Context/SocketContext";
-import { useNavigate } from "react-router-dom";
-import { UserContext } from "../Shared/Context/UserContext";
-import { CardGame } from "./Components/Card";
-const Index = () => {
-  const [rooms, setRooms] = useState();
-  const [roomName, setRoomName] = useState("");
-  const [playersName, setPlayersName] = useState("");
-  const [maxPlayer, setMaxPlayer] = useState("");
-  const [roomPassword, setRoomPassword] = useState("");
-  const navigation = useNavigate();
-  const { socket } = useContext(SocketContext);
-  const { playerName, setPlayerName } = useContext(UserContext);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (socket != null) {
-        socket.emit("getRooms");
-        socket.emit("getPlayersName");
-        socket.on("playersName", (data) => {
-
-        })
-        socket.on("rooms", (data) => {
-          setRooms(data);
-        });
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [socket]);
-
-  const joinRoom = (roomName) => {
-    socket.emit("joinRoom", { playerName, roomName, roomPassword });
-    socket.on("roomJoined", () => {
-      navigation(`/game/${roomName}`);
-    });
-  };
-
-  return (
-    <>
-      <div className="grid grid-cols-3 gap-4 xl:grid-cols-6 auto-rows-max">
-        {rooms !== undefined
-          ? Object.keys(rooms).map((roomName) => (
-              <CardGame
-                key={roomName}
-                players={rooms[roomName].players}
-                roomName={roomName}
-                playersNumber={rooms[roomName].players.length}
-                maxPlayer={rooms[roomName].maxPlayer}
-                onClick={() => joinRoom(roomName)}
-              />
-            ))
-          : null}
+import React from "react";
+import { Link } from "react-router-dom";
+const Index = () => (
+  <div class="flex items-center justify-center h-full ">
+    <div class="container">
+      <div class="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+        <div class="text-center">
+          <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+            This page is coming soon !
+          </h2>
+          <h3 class="text-xl md:text-3xl mt-10">
+            Check lobby page for now :{" "}
+            <Link to="/lobby">
+              <button class="bg-transparent hover:bg-teal-600 text-teal-400 font-semibold hover:text-white py-2 px-4 border border-teal-400 hover:border-transparent rounded mr-2">
+                Go for lobbys !
+              </button>
+            </Link>
+          </h3>
+        </div>
       </div>
-      <div className="py-4">
-        <input
-          placeholder="Password of the room"
-          value={roomPassword}
-          onChange={(event) => setRoomPassword(event.target.value)}
-        ></input>
-        <input
-          placeholder="Number max of player for room"
-          value={maxPlayer}
-          onChange={(event) => setMaxPlayer(event.target.value)}
-        ></input>
-        <input
-          placeholder="Name of the room"
-          value={roomName}
-          onChange={(event) => setRoomName(event.target.value)}
-        ></input>
-        <input
-          placeholder="Name of the player"
-          value={playerName}
-          onChange={(event) => setPlayerName(event.target.value)}
-        ></input>
-        <button
-          onClick={() => {
-            socket.emit("createRoom", { playerName, roomName, maxPlayer });
-            navigation(`/game/${roomName}`);
-          }}
-        >
-          Create a room
-        </button>
-      </div>
-    </>
-  );
-};
+    </div>
+  </div>
+);
 
 export { Index };
---
+++ rpg-socket-client/src/Pages/Lobby/Lobby.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState, useContext } from "react";
+import { SocketContext } from "../../Shared/Context/SocketContext";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../Shared/Context/UserContext";
+import { CardGame } from "./Components/Card";
+import { useModal } from "../../Shared/Hooks/ModalHooks";
+import { CreateRoomForm } from "./Components/CreateRoomForm";
+import { JoinRoom } from "./Components/JoinRoom";
+import { PlayerNameForm } from "./Components/PlayerNameForm";
+
+const Lobby = () => {
+  const [rooms, setRooms] = useState();
+  const navigation = useNavigate();
+  const { socket } = useContext(SocketContext);
+  const { playerName, setPlayerName, checkPlayerNameExists } = useContext(
+    UserContext
+  );
+  const { setIsModalOpen, Modal, ShowAndSetModalContent } = useModal();
+
+  const addOrModifyPlayerName = (type) => {
+    ShowAndSetModalContent(
+      `${type} your player Name`,
+      <PlayerNameForm
+        setIsModalOpen={setIsModalOpen}
+        setPlayerName={setPlayerName}
+        playerName={playerName}
+      />
+    );
+  };
+
+  useEffect(() => {
+    if (!checkPlayerNameExists()) {
+      addOrModifyPlayerName("Insert");
+    }
+    const interval = setInterval(() => {
+      if (socket != null) {
+        socket.emit("getRooms");
+        socket.emit("getPlayersName");
+        socket.on("playersName", (data) => {});
+        socket.on("rooms", (data) => {
+          setRooms(data);
+        });
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket]);
+
+  const joinRoom = (roomName, roomPassword) => {
+    socket.emit("joinRoom", { playerName, roomName, roomPassword });
+    socket.on("roomJoined", () => {
+      navigation(`/game/${roomName}`);
+    });
+  };
+
+  const createRoom = (roomName, maxPlayer) => {
+    socket.emit("createRoom", { playerName, roomName, maxPlayer });
+    socket.on("roomCreated", () => {
+      navigation(`/game/${roomName}`);
+    });
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4 xl:grid-cols-6 auto-rows-max">
+        {rooms !== undefined
+          ? Object.keys(rooms).map((roomName) => (
+              <CardGame
+                key={roomName}
+                players={rooms[roomName].players}
+                roomName={roomName}
+                playersNumber={rooms[roomName].players.length}
+                maxPlayer={rooms[roomName].maxPlayer}
+                onClick={() =>
+                  ShowAndSetModalContent(
+                    `Joining : ${roomName}`,
+                    <JoinRoom
+                      key={roomName}
+                      joinRoom={joinRoom}
+                      roomName={roomName}
+                    />
+                  )
+                }
+              />
+            ))
+          : null}
+      </div>
+      {rooms !== undefined && Object.keys(rooms).length === 0 ? (
+        <h1 className="p-2 text-2xl font-bold text-center">
+          Currently no room !
+        </h1>
+      ) : null}
+      <div className="w-full py-4">
+        <button
+          className="flex items-center justify-center w-full h-8 p-2 my-2 text-white bg-blue-300 rounded hover:bg-blue-500"
+          onClick={() =>
+            ShowAndSetModalContent(
+              "Create a room",
+              <CreateRoomForm createRoom={createRoom} />
+            )
+          }
+        >
+          Create a room
+        </button>
+        <button
+          className="flex items-center justify-center w-full h-8 p-2 m-2 text-white bg-blue-300 rounded hover:bg-blue-500"
+          onClick={() => addOrModifyPlayerName("Modify")}
+        >
+          Modify player name
+        </button>
+        {Modal}
+      </div>
+    </>
+  );
+};
+
+export { Lobby };
