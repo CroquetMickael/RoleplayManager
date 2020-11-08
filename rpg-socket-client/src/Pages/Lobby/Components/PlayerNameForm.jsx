86033cda01e34eb21516002b7e767f31427c6b89
unknown <mickael.croquet@laposte.net>
2020-11-08 17:23:45 +0100

    fix(player): fixing playerName form not updating.

--- rpg-socket-client/src/Pages/Lobby/Components/PlayerNameForm.jsx
+++ rpg-socket-client/src/Pages/Lobby/Components/PlayerNameForm.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const PlayerNameForm = ({ setIsModalOpen, setPlayerName, playerName }) => (
-  <div className="flex flex-col">
-    <input
-      className="p-2 mb-1 border border-black"
-      placeholder="Please insert your player name"
-      value={playerName}
-      onChange={(event) => setPlayerName(event.target.value)}
-    ></input>
-    <button
-      className="flex items-center justify-center w-full h-8 p-2 text-white bg-blue-300 rounded hover:bg-blue-500"
-      onClick={() => setIsModalOpen(false)}
-    >
-      Save change
-    </button>
-  </div>
-);
+const PlayerNameForm = ({ setIsModalOpen, setPlayerName, playerName }) => {
+  const [localPlayerName, setLocalPlayerName] = useState("");
+
+  useEffect(() => {
+    if (playerName !== "" || null) {
+      setLocalPlayerName(playerName);
+    }
+  }, [playerName]);
+
+  const onSave = () => {
+    setPlayerName(localPlayerName);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <input
+        className="p-2 mb-1 border border-black"
+        placeholder="Please insert your player name"
+        value={localPlayerName}
+        onChange={(event) => setLocalPlayerName(event.target.value)}
+      ></input>
+      <button
+        className="flex items-center justify-center w-full h-8 p-2 text-white bg-blue-300 rounded hover:bg-blue-500"
+        onClick={() => onSave()}
+      >
+        Save change
+      </button>
+    </div>
+  );
+};
 
 export { PlayerNameForm };
